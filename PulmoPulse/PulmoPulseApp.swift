c19ffeeb360fb557ae75cefa4140f1c9deaf38cf
wolfgangkle <wolfgang.kleinhaentz@gmail.com>
2025-06-30 16:18:15 +0200

    Initial Commit

---
+++ PulmoPulse/PulmoPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulmoPulseApp.swift
+//  PulmoPulse
+//
+//  Created by Wolfgang Kleinhaentz on 30/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct PulmoPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PulmoPulseTests/PulmoPulseTests.swift
@@ -0,0 +1,17 @@
+//
+//  PulmoPulseTests.swift
+//  PulmoPulseTests
+//
+//  Created by Wolfgang Kleinhaentz on 30/06/2025.
+//
+
+import Testing
+@testable import PulmoPulse
+
+struct PulmoPulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
